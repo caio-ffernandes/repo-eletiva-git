b05fd151629de2b43900932718c4d3d772414d84
Pedro Joaquim <pjbrito2016@gmail.com>
2026-09-03 11:11:01 -0300

    ajeitando

--- novo.php
+++ novo.php
@@ -3,7 +3,13 @@
     echo "Ola Git!";
     echo "Ola Github!";
 
-    function divisiao($num1, $num2){
-        return ($num1/$num2);
+    function divisiao($num1, $num2)
+    {
+        if($num2 == 0)
+        {
+            return "indivisível";
+        }
+    return ($num1/$num2);
+    
     }
     ?>
